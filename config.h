25ba9653397c065c783d7c096f4d1dc6ec865441
Mikepicker <michele.rullo90@gmail.com>
2018-10-23 13:20:19 +0200

    [*] mike fork

--- config.h
+++ config.h
@@ -1,16 +1,16 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int borderpx  = 16;        /* border pixel of windows */
+static const unsigned int snap      = 0;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "inconsolata:size=9" };
+static const char dmenufont[]       = "inconsolata:size=9";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -20,14 +20,26 @@ static const char nord_0[]          = "#2e3440";
 static const char nord_1[]          = "#3b4252";
 static const char nord_2[]          = "#434c5e";
 static const char nord_3[]          = "#4c566a";
+static const char nord_4[]          = "#d8dee9";
+static const char nord_5[]          = "#e5e9f0";
+static const char nord_6[]          = "#eceff4";
+static const char nord_7[]          = "#8fbcbb";
+static const char nord_8[]          = "#88c0d0";
+static const char nord_9[]          = "#81a1c1";
+static const char nord_10[]         = "#5e81ac";
+static const char nord_11[]         = "#bf6161";
+static const char nord_12[]         = "#d08770";
+static const char nord_13[]         = "#ebcb8b";
+static const char nord_14[]         = "#a3be8c";
+static const char nord_15[]         = "#b48ead";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { nord_1, nord_0, nord_2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { nord_3, nord_0,  nord_11 },
+	[SchemeSel]  = { nord_4, nord_0,  nord_1 },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "One", "Two", "Three", "Four" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -42,7 +54,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -66,6 +78,11 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+
+/* Volume ALT + F11 / F12 */
+static const char *upvol[]   = { "amixer", "-q", "sset", "Master", "3%+",     NULL  };
+static const char *downvol[] = { "amixer", "-q", "sset", "Master", "3%-",     NULL  };
+static const char *mutevol[] = { "amixer", "-q", "sset", "Master", "toggle", NULL  };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -92,6 +109,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+  { MODKEY,                       XK_F12,    spawn,          {.v = upvol   }  },
+  { MODKEY,                       XK_F11,    spawn,          {.v = downvol }  },
+  { MODKEY,                       XK_F9,     spawn,          {.v = mutevol }  },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -120,4 +140,5 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
+
 
