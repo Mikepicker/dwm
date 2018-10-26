d587ae5896e30926def43dc2365c008a9a5f8e87
Mikepicker <michele.rullo90@gmail.com>
2018-10-26 10:04:39 +0200

    [-] removed window title

--- config.h
+++ config.h
@@ -10,7 +10,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Fira Mono Medium:size=9", "Font Awesome 5 Free:size=9" };
+// static const char *fonts[]          = { "FontAwesome", "Fira Mono Medium:size=9" };
+static const char *fonts[]          = { "Wuncon Siji:size=9", "Fira Mono Medium:size=9" };
 static const char dmenufont[]       = "Fira Mono Medium:size=9";
 static const char nord_0[]          = "#2e3440";
 static const char nord_1[]          = "#3b4252";
